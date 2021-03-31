4c2e2586b6929cc89319e63afb0c37596b8be3d0
Kirima2nd <adamanyyouth12@gmail.com>
2021-04-01 02:38:27 +0700

    Initial Release

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using LZStringCSharp;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace RPGMV_Save_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                                  ");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("| RPGMaker Save Converter v0.1.2 |");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("                                  ");
+            ///(file)(\d+)[.](rpgsave)
+            if (args.Length == 0)
+            {
+                Console.WriteLine("ERROR: Invalid path file.");
+                Console.WriteLine("----------------------------------");
+                return;
+            }
+
+            
+            if (!Regex.Match(Path.GetFileName(args[0]), @"(file)(\d+)").Success)
+            {
+                Console.WriteLine("ERROR: Invalid file name");
+                Console.WriteLine("----------------------------------");
+                return;
+            }
+
+            if (Path.GetExtension(args[0]) == ".rpgsave")
+            {
+                Console.WriteLine("INFO: Decoding the file...");
+                DecodeSave(args[0]);
+            }
+            else if (Path.GetExtension(args[0]) == ".json")
+            {
+                Console.WriteLine("INFO: Encoding the file...");
+                EncodeSave(args[0]);
+            }
+            else {
+                Console.WriteLine("ERROR: Invalid file name");
+                Console.WriteLine("----------------------------------");
+                return;
+            }
+        }
+        static void DecodeSave(string filePath)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            string textFile = LZString.DecompressFromBase64(sr.ReadToEnd());
+            sr.Close();
+
+            string savePath = Path.GetDirectoryName(filePath)
+                                + Path.DirectorySeparatorChar
+                                + Path.GetFileNameWithoutExtension(filePath)
+                                + ".json"
+                                ;
+
+            File.WriteAllText(savePath, PretifyJSON(textFile));
+
+            Console.WriteLine("INFO: Success decoding the file.");
+            Console.WriteLine("----------------------------------");
+        }
+        static void EncodeSave(string filePath)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            string textFile = sr.ReadToEnd();
+            sr.Close();
+
+            string EncodeFile = LZString.CompressToBase64(MinifyJSON(textFile));
+
+            string savePath = Path.GetDirectoryName(filePath)
+                                + Path.DirectorySeparatorChar
+                                + Path.GetFileNameWithoutExtension(filePath)
+                                + ".rpgsave"
+                                ;
+
+            File.WriteAllText(savePath, EncodeFile);
+ 
+            Console.WriteLine("INFO: Success encoding the file.");
+            Console.WriteLine("----------------------------------");
+        }
+        private static string PretifyJSON(string str)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(str);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+        }
+
+        private static string MinifyJSON(string str)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(str);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.None);
+        }
+}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RPGMaker Save Tool")]
+[assembly: AssemblyDescription("Decoder and Reencode the rpgsave!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RPGMaker-Save-Tool")]
+[assembly: AssemblyCopyright("Copyright © Kirima 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de4a9f15-bbc8-473c-9cfb-ebbd6982d449")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2")]
